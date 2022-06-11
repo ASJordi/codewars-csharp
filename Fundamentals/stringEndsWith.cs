3f2ff5f5880c6c07f0ad8706b7cac7883159df6b
Jordi Ayala <jord7as@gmail.com>
2022-06-11 12:25:37 -0500

    String Ends With

---
+++ Fundamentals/stringEndsWith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    public class stringEndsWith
+    {
+        /// <summary>
+        /// Complete the solution so that it returns true if the first argument(string) 
+        /// passed in ends with the 2nd argument (also a string).
+        /// Examples
+        /// solution('abc', 'bc') returns true
+        /// solution('abc', 'd') returns false
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="ending"></param>
+        /// <returns></returns>
+        /// 
+        public static bool endsWith(string str, string ending)
+        {
+            if (str.EndsWith(ending))
+                return true;
+            else
+                return false;
+            //return str.EndsWith(ending);
+        }
+    }
+}
---
+++ FundamentalsTests/stringEndsWithTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace FundamentalsTests
+{
+    [TestClass]
+    public class stringEndsWithTest
+    {
+        public static IEnumerable<object[]> sampleTestCases
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] {"samurai", "ai", true},
+                    new object[] {"sumo", "omo", false},
+                    new object[] {"ninja", "ja", true},
+                    new object[] {"sensei", "i", true},
+                    new object[] {"samurai", "ra", false},
+                    new object[] {"abc", "abcd", false},
+                    new object[] {"abc", "abc", true},
+                    new object[] {"abcabc", "bc", true},
+                    new object[] {"ails", "fails", false},
+                    new object[] {"fails", "ails", true},
+                    new object[] {"this", "fails", false},
+                    new object[] {"abc", "", true},
+                    new object[] {":-)", ":-(", false},
+                    new object[] {"!@#$%^&*() :-)", ":-)", true},
+                    new object[] {"abc\n", "abc", false},
+
+                };
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(sampleTestCases))]
+        public void SampleTest(string str, string ending, bool expected)
+        {
+            Assert.AreEqual(expected, Fundamentals.stringEndsWith.endsWith(str, ending));
+        }
+    }
+}
