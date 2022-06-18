1f6ef835a89e2a34e312f66f52a9c9801180c7da
Jordi Ayala <jord7as@gmail.com>
2022-06-18 15:19:38 -0500

    Get the middle character

---
+++ Fundamentals/getTheMiddleCharacter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    public class getTheMiddleCharacter
+    {
+        /// <summary>
+        /// You are going to be given a word. 
+        /// Your job is to return the middle character of the word. 
+        /// If the word's length is odd, return the middle character. 
+        /// If the word's length is even, return the middle 2 characters.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string GetMiddle(string str)
+        {
+            List<string> list = new List<string>();
+            foreach (var item in str)
+            {
+                list.Add(item.ToString());
+            }
+
+            int length = str.Length;
+            if ((length % 2) == 0)
+                return list[(length / 2) - 1] + list[length / 2];
+            else
+                return list[length / 2];
+
+            //return s.Length % 2 == 0 ? s.Substring(s.Length / 2 - 1, 2) : s.Substring(s.Length / 2, 1);
+
+        }
+    }
+}
---
+++ FundamentalsTests/getTheMiddleCharacterTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsTests
+{
+    [TestClass]
+    public class getTheMiddleCharacterTest
+    {
+        [TestMethod]
+        public void GenericTests()
+        {
+            Assert.AreEqual("es", Fundamentals.getTheMiddleCharacter.GetMiddle("test"));
+            Assert.AreEqual("t", Fundamentals.getTheMiddleCharacter.GetMiddle("testing"));
+            Assert.AreEqual("dd", Fundamentals.getTheMiddleCharacter.GetMiddle("middle"));
+            Assert.AreEqual("A", Fundamentals.getTheMiddleCharacter.GetMiddle("A"));
+        }
+    }
+}
