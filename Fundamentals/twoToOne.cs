1c2652c42fd9cad02ccf18aa282cec845d442a5f
Jordi Ayala <jord7as@gmail.com>
2022-06-18 14:57:12 -0500

    Two to One

---
+++ Fundamentals/twoToOne.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    public class twoToOne
+    {
+        /// <summary>
+        /// Take 2 strings s1 and s2 including only letters from a to z. 
+        /// Return a new sorted string, the longest possible, 
+        /// containing distinct letters - each taken only once - coming from s1 or s2.
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        public static string Longest(string s1, string s2)
+        {
+            string str = s1 + s2;
+
+            List<string> unsortedList = new List<string>();
+            foreach (var item in str)
+            {
+                unsortedList.Add(item.ToString());
+            }
+
+            List<string> orderList = unsortedList.Distinct().OrderBy(letter => letter).ToList();
+
+            return string.Join("", orderList);
+            //return string.Concat((s1 + s2).Distinct().OrderBy(c => c));
+        }
+    }
+}
---
+++ FundamentalsTests/twoToOneTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsTests
+{
+    [TestClass]
+    public class twoToOneTest
+    {
+        [TestMethod]
+        public void TestFixed()
+        {
+            Assert.AreEqual("aehrsty", Fundamentals.twoToOne.Longest("aretheyhere", "yestheyarehere"));
+            Assert.AreEqual("abcdefghilnoprstu", Fundamentals.twoToOne.Longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+            Assert.AreEqual("acefghilmnoprstuy", Fundamentals.twoToOne.Longest("inmanylanguages", "theresapairoffunctions"));
+            Assert.AreEqual("abcdefklmopqwxy", Fundamentals.twoToOne.Longest("xyaabbbccccdefww", "xxxxyyyyabklmopq"));
+            Assert.AreEqual("abcdefghijklmnopqrstuvwxyz", Fundamentals.twoToOne.Longest("abcdefghijklmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz"));
+        }
+    }
+}
