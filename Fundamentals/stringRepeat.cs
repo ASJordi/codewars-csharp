4bf99860e00c9e2d62aa791b8b642ea15c323cbc
Jordi Ayala <jord7as@gmail.com>
2022-06-18 14:12:43 -0500

    String repeat

---
+++ Fundamentals/stringRepeat.cs
@@ -0,0 +1,19 @@
+﻿namespace Fundamentals
+{
+    public class stringRepeat
+    {
+        /// <summary>
+        /// Write a function that accepts an integer n and a string s as parameters, 
+        /// and returns a string of s repeated exactly n times.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string RepeatStr(int n, string s)
+        {
+            return string.Concat(Enumerable.Repeat(s, n));
+            //6, "I"     -> "IIIIII"
+            //5, "Hello" -> "HelloHelloHelloHelloHello"
+        }
+    }
+}
---
+++ FundamentalsTests/stringRepeatTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsTests
+{
+    [TestClass]
+    public class stringRepeatTest
+    {
+        [TestMethod]
+        public void TestFixed()
+        {
+            Assert.AreEqual("***", Fundamentals.stringRepeat.RepeatStr(3, "*"));
+            Assert.AreEqual("#####", Fundamentals.stringRepeat.RepeatStr(5, "#"));
+            Assert.AreEqual("ha ha ", Fundamentals.stringRepeat.RepeatStr(2, "ha "));
+        }
+    }
+}
